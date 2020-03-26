15160673be3b1be6fb5074be1d73128e88e1daed
sanathkashyap19 <sanathkashyap92@gmail.com>
2020-03-26 23:14:58 +0530

    changes the carousel and flip card images, made carousel indicators dynamic, fixed reponsiveness of flip cards.

--- src/components/mixin-bootstrap-carousel/mixin-bootstrap-carousel.js
+++ src/components/mixin-bootstrap-carousel/mixin-bootstrap-carousel.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
     $('.carousel .carousel-inner .carousel-item:first-child').addClass('active');
+    $('.carousel .carousel-indicators li:first-child').addClass('active');
 })
